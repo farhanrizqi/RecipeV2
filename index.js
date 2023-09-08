a77efb354e960fef2413e162ad563ae72ae27d0f
farhan <farhanrizqi008@gmail.com>
2023-09-08 07:12:17 +0700

    cors fixing

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const router = require("./src/router");
-const cors = require("./src/middleware/cors");
+const cors = require("cors");
 const helmet = require("./src/middleware/helmet");
 const xss = require("./src/middleware/xss");
 const morgan = require("./src/middleware/morgan");
 require("dotenv").config();
 
 // app use
-app.use(cors);
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(helmet);
 app.use("/img", express.static("assets"));
 app.use(morgan);
--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -4,6 +4,8 @@ const Cors = require("cors");
 // CORS configuration
 const corsOptions = {
   origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Atur metode yang diizinkan
+  credentials: true, // Izinkan kredensial (misalnya, cookie) dalam permintaan
   optionsSuccessStatus: 200,
 };
 
